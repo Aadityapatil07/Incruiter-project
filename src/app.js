6e9b4dd34d8b06994f07f93ae81200a642277632
Aaditya Patil <aadityapatil081@gmail.com>
2025-03-05 12:51:07 +0530

    Complete Project

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST"]
+}))
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+
+// http://localhost:8000/api/v1/users/register
+export { app }
